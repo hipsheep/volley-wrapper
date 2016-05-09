0674f4cbbfee1bb385889e0deb225f3891dee42e
Franco Sabadini <fsabadi@gmail.com>
2016-05-10 00:11:14 +1200

    Added instrumentation tests for the async sendRequest() method.

--- volleywrapper/src/androidTest/java/com/hipsheep/volleywrapper/VolleyWrapperTest.java
+++ volleywrapper/src/androidTest/java/com/hipsheep/volleywrapper/VolleyWrapperTest.java
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.android.volley.VolleyError;
+import com.hipsheep.volleywrapper.network.model.ResponseCallback;
 import com.hipsheep.volleywrapper.network.request.GetRequest;
 import com.hipsheep.volleywrapper.network.request.PostRequest;
 import com.hipsheep.volleywrapper.network.model.Post;
@@ -52,17 +53,47 @@ public class VolleyWrapperTest {
 	@Test
 	public void sendRequest_GET_Sync_Success() {
 		int userId = 1;
-		Post[] response = null;
+		Post[] posts = null;
 
 		try {
-			response = VolleyWrapper.sendRequest(new GetRequest(TEST_URL_SUCCESS, userId));
+			posts = VolleyWrapper.sendRequest(new GetRequest(TEST_URL_SUCCESS, userId));
 		} catch (Exception e) {
 			Log.e(TAG, "GET request failed", e);
 		}
 
-		Assert.assertNotNull(response);
+		assertGetResponse(posts, userId);
+	}
 
-		for (Post p : response) {
+	@Test
+	public void sendRequest_GET_Async_Success() {
+		final int userId = 1;
+
+		VolleyWrapper.sendRequest(new GetRequest(TEST_URL_SUCCESS, userId), new ResponseCallback<Post[]>() {
+
+			@Override
+			public void onSuccess(Post[] posts) {
+				assertGetResponse(posts, userId);
+			}
+
+			@Override
+			public void onFailure(VolleyError volleyError) {
+				Log.e(TAG, "GET request failed", volleyError);
+
+				Assert.fail();
+			}
+
+		});
+
+		waitForResponse();
+	}
+
+	/**
+	 * Asserts the successful response from the GET requests is correct.
+	 */
+	private void assertGetResponse(Post[] posts, int userId) {
+		Assert.assertNotNull(posts);
+
+		for (Post p : posts) {
 			if (p.getUserId() != userId) {
 				Assert.fail();
 			}
@@ -77,25 +108,76 @@ public class VolleyWrapperTest {
 			// We are testing that the request should return an error, so if it doesn't then the test failed
 			Assert.fail();
 		} catch (Exception e) {
-			assertError(404, e);
+			assertError(404, e.getCause());
 		}
 	}
 
 	@Test
+	public void sendRequest_GET_Async_Fail_404() {
+		VolleyWrapper.sendRequest(new GetRequest(TEST_URL_FAIL, null), new ResponseCallback<Post[]>() {
+
+			@Override
+			public void onSuccess(Post[] posts) {
+				// We are testing that the request should return an error, so if it doesn't then the test failed
+				Assert.fail();
+			}
+
+			@Override
+			public void onFailure(VolleyError volleyError) {
+				assertError(404, volleyError);
+			}
+
+		});
+
+		waitForResponse();
+	}
+
+	@Test
 	public void sendRequest_POST_Sync_Success() {
-		Post response = null;
+		Post post = null;
 
 		try {
-			response = VolleyWrapper.sendRequest(new PostRequest(TEST_URL_SUCCESS, TEST_POST_MODEL.getUserId(),
+			post = VolleyWrapper.sendRequest(new PostRequest(TEST_URL_SUCCESS, TEST_POST_MODEL.getUserId(),
 					TEST_POST_MODEL.getTitle(), TEST_POST_MODEL.getBody()));
 		} catch (Exception e) {
 			Log.e(TAG, "POST request failed", e);
 		}
 
-		// Set post ID which is generated randomly by the backend (needed for the equals() method to work)
-		TEST_POST_MODEL.setId(response.getId());
+		assertPostResponse(post);
+	}
 
-		Assert.assertEquals(TEST_POST_MODEL, response);
+	@Test
+	public void sendRequest_POST_Async_Success() {
+		VolleyWrapper.sendRequest(new PostRequest(TEST_URL_SUCCESS, TEST_POST_MODEL.getUserId(), TEST_POST_MODEL.getTitle(),
+				TEST_POST_MODEL.getBody()), new ResponseCallback<Post>() {
+
+			@Override
+			public void onSuccess(Post post) {
+				assertPostResponse(post);
+			}
+
+			@Override
+			public void onFailure(VolleyError volleyError) {
+				Log.e(TAG, "POST request failed", volleyError);
+
+				Assert.fail();
+			}
+
+		});
+
+		waitForResponse();
+	}
+
+	/**
+	 * Asserts the successful response from the POST requests is correct.
+	 */
+	private void assertPostResponse(Post postResponse) {
+		Assert.assertNotNull(postResponse);
+
+		// Set post ID which is generated randomly by the backend (needed for the equals() method to work)
+		TEST_POST_MODEL.setId(postResponse.getId());
+
+		Assert.assertEquals(TEST_POST_MODEL, postResponse);
 	}
 
 	@Test
@@ -106,14 +188,53 @@ public class VolleyWrapperTest {
 			// We are testing that the request should return an error, so if it doesn't then the test failed
 			Assert.fail();
 		} catch (Exception e) {
-			assertError(404, e);
+			assertError(404, e.getCause());
 		}
 	}
 
-	private void assertError(int errorCode, Exception e) {
-		Log.e(TAG, "Request failed", e);
+	@Test
+	public void sendRequest_POST_Async_Fail_404() {
+		VolleyWrapper.sendRequest(new PostRequest(TEST_URL_FAIL, null), new ResponseCallback<Post>() {
 
-		Assert.assertEquals(errorCode, ((VolleyError) e.getCause()).networkResponse.statusCode);
+			@Override
+			public void onSuccess(Post post) {
+				// We are testing that the request should return an error, so if it doesn't then the test failed
+				Assert.fail();
+			}
+
+			@Override
+			public void onFailure(VolleyError volleyError) {
+				assertError(404, volleyError);
+			}
+
+		});
+
+		waitForResponse();
+	}
+
+	/**
+	 * Asserts whether the error code inside the exception is the same as the one sent as parameter.
+	 */
+	private void assertError(int errorCode, Throwable volleyError) {
+		Log.e(TAG, "Request failed", volleyError);
+
+		if (volleyError instanceof VolleyError) {
+			Assert.assertEquals(errorCode, ((VolleyError) volleyError).networkResponse.statusCode);
+		} else {
+			Assert.fail();
+		}
+	}
+
+	/**
+	 * Puts the current thread to sleep, which is needed for async responses to be able to return a value asynchronously without
+	 * the test ending.
+	 */
+	private void waitForResponse() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
